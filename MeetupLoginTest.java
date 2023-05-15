4b0a001d699b68a8b70e7312496767298d15f84f
Matt <m_alizd@yahoo.com>
2023-05-15 19:03:06 +1000

    As MobileElement deprecated in java client v8.30 and the way Desired capabilities defined is changed, creating dev branch to implement those changes and refactoring

--- MeetupLoginTest.java
+++ MeetupLoginTest.java
@@ -1,4 +1,5 @@
 package appiumtests;
+import base.AppDriver;
 import java.util.Scanner;
 
 import org.openqa.selenium.By;
@@ -18,24 +19,66 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class CalculatorTest {
+public class MeetupLoginTest {
 
-	//WebDriver driver;		// for web browser test
-	static AppiumDriver driver;
-	//AndroidDriver driver;
+    public static AppiumDriver driver;
+    public static DesiredCapabilities cap;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			try {
-				testlogin();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.getCause();
-				e.getMessage();
-				e.printStackTrace();
-			}
+
+		UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("SM-G973F")
+                .setPlatformVersion("12.0")
+                .setAppPackage("com.meetup")
+                .setAppActivity(".feature.legacy.activity.BootstrapActivity");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+
+        AppDriver.setDriver(driver);
+        Thread.sleep(1000);
+
+		// Positive testcase
+		driver.AppiumBy.accessibilityId("intro_login_link_button").click();
+		//loginButton.click();
+
+		driver.AppiumBy.accessibilityId("login_email").sendKeys("test1valid@yahoo.com");
+		// MobileElement emailInput = (MobileElement) driver.findElementById("com.meetup:id/login_email");
+		// emailInput.sendKeys("test1valid@yahoo.com");
+
+		driver.AppiumBy.accessibilityId("login_password").sendKeys("MyPass_1");
+		// MobileElement passwordInput = (MobileElement) driver.findElementById("com.meetup:id/login_password");
+		// passwordInput.sendKeys("MyPass_1");
+
+		driver.AppiumBy.accessibilityId("email_login_button").click();
+		// MobileElement signInButton = (MobileElement) driver.findElementById("com.meetup:id/email_login_button");
+		// signInButton.click();
+
+        Thread.sleep(10000);
+        MobileElement succeed = (MobileElement) driver.findElementById("com.meetup:id/upgrade_button");
+
+        // Assert
+        String res = succeed.getText();
+		if (res == "Welcome!")
+			System.out.println("\nTestcase failed"); 	// False positive
+		else
+			System.out.println("\nTestcase passed");
+	
+	// ToDo: loguut();	// get prepared for another login attempt
+		
+	System.out.println("\nEnd of tests");
+	driver.quit();
+
+		// try {
+		// 		testlogin();
+		// 	} catch (Exception e) {
+		// 		// TODO Auto-generated catch block
+		// 		e.getCause();
+		// 		e.getMessage();
+		// 		e.printStackTrace();
+		// 	}
 	}
-	public static void testlogin() throws Exception {
+/* 	public static void testlogin() throws Exception {
 		//deviceName, udid, 
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("deviceName", "SM-G973F");
@@ -50,18 +93,6 @@ public class CalculatorTest {
 		driver = new AppiumDriver(url,cap);
 		System.out.println("Meetup App test started...");
 		
-// Positive testcase
-        MobileElement loginButton = (MobileElement) driver.findElementById("com.meetup:id/intro_login_link_button");
-        loginButton.click();
-
-        MobileElement emailInput = (MobileElement) driver.findElementById("com.meetup:id/login_email");
-        emailInput.sendKeys("test1valid@yahoo.com");
-
-        MobileElement passwordInput = (MobileElement) driver.findElementById("com.meetup:id/login_password");
-        passwordInput.sendKeys("MyPass_1");
-
-        MobileElement signInButton = (MobileElement) driver.findElementById("com.meetup:id/email_login_button");
-        signInButton.click();
 		
 	// ToDo: a 10 seconds delay
 
@@ -69,7 +100,7 @@ public class CalculatorTest {
 	// Todo: Handling an edge case where upon a successful login, Meetup pops up a screen asking for rating an attended event or some notification
 		
 	// When landed at Homepage screen
-        MobileElement succeed = (MobileElement) driver.findElementById("com.meetup:id/upgrade_button");
+//        MobileElement succeed = (MobileElement) driver.findElementById("com.meetup:id/upgrade_button");
 //        Assert
 		String res = succeed.getText();
 		if (res == "Welcome!")
@@ -94,19 +125,7 @@ public class CalculatorTest {
 
 	// ToDo: a 10 seconds delay
 		
-        MobileElement succeed = (MobileElement) driver.findElementById("com.meetup:id/upgrade_button");
-
-        // Assert
-        String res = succeed.getText();
-		if (res == "Welcome!")
-			System.out.println("\nTestcase failed"); 	// False positive
-		else
-			System.out.println("\nTestcase passed");
-	
-	// ToDo: loguut();	// get prepared for another login attempt
-		
-	System.out.println("\nEnd of tests");
 
 	}
+ */
 }
-
